a038c77808a0d1ed01493a7fbedfae9a0e61d0d1
Suwen macOS Yurisa <577014284@qq.com>
2020-08-31 23:34:42 +0800

    更新apiUrl接口

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://47.107.239.108:8090/cssl',
-    // apiUrl: 'http://localhost:8090/cssl',
+    apiUrl: 'https://suwen.live/cssl',
     filePreviewUrl: 'http://47.107.239.108:8012'
 };
